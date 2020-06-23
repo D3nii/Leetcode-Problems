fa72165ffa9387775330488cbe65dcd639011a07
Danyal Jamil <danyal.jcc@gmail.com>
2020-06-23 09:58:09 +0300

    Create Search_Insert_Position.js

---
+++ JavaScript/Search_Insert_Position.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] === target) return i;
+    }
+    
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] > target) return i;
+    }
+    
+    return nums.length;
+};
