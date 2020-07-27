b50d7df1f14982e01f8ec85743573c547f063d17
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-27 05:18:56 +0300

    Create Assign_Cookies.js

---
+++ JavaScript/Assign_Cookies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a, b) => a-b);
+    s.sort((a, b) => a-b);
+    let temp = 0;
+    
+    for (let i=0, j=0; i<g.length, j<s.length; i++, j++) {
+        if (g[i] <= s[j]) temp++;
+        else i--;
+    }
+    
+    return temp;
+};
