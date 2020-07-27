fcffdfe873e37ffe7687b1e7b14628b94bbcd322
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-27 05:19:03 +0300

    Create Best_Time_to_Buy_and_Sell_Stocks.js

---
+++ JavaScript/Best_Time_to_Buy_and_Sell_Stocks.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0, min = Infinity;
+    
+    for (let i=0; i<prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+    }
+    
+    return max;
+};
