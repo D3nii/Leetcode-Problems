bb85eda67cffea50ff69f1a0dc68fdc267fe2621
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-29 03:00:43 +0300

    Create Pow(x,_n).js

---
+++ JavaScript/Pow(x,_n).js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n == 0) return 1;
+    else if (n == 1) return x;
+    else if (n == -1) return 1/x;
+    
+    else if (n % 2 == 0) {
+        let temp = myPow(x, n/2);
+        return temp * temp;
+    }
+    else return x * myPow(x, n-1);
+};
